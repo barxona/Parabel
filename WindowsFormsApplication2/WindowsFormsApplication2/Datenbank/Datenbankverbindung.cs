13dd4431d788016fde65c3d07c034569938bfb44
barxona <32041917+barxona@users.noreply.github.com>
2017-10-09 18:35:42 +0200

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Datenbank/Datenbankverbindung.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApplication2.Datenbank
+{
+    class Datenbankverbindung
+    {
+        public static string ConnectionString =
+    "SERVER=" + "sql11.freemysqlhosting.net" + "; DATABASE=" + "sql11198551" + "; UID=" + "	sql11198551" +
+    "; PASSWORD=" + "tEaJcgq2qh";
+
+        public static long Query(string Query)
+        {
+            using (var conn = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = Query;
+                    cmd.ExecuteNonQuery();
+
+                    conn.Close();
+
+                    return cmd.LastInsertedId;
+                }
+                catch (MySqlException e)
+                {
+                    MessageBox.Show("[MySQL][Error]" + e);
+                    return -1;
+                }
+            }
+        }
+
+        public static DataTable QueryResult(string Query)
+        {
+            using (var conn = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = Query;
+                    var reader = cmd.ExecuteReader();
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return null;
+                    }
+                    var reslut = new DataTable();
+                    reslut.Load(reader);
+                    reader.Close();
+
+                    conn.Close();
+
+                    return reslut;
+                }
+                catch (MySqlException e)
+                {
+                    MessageBox.Show("[MySQL][Error]" + e);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using WindowsFormsApplication2.Datenbank;
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sql()
+        {
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text.ToString();
+            label2.Text = textBox2.Text.ToString();
+            var hasUser =
+           Datenbankverbindung.QueryResult(
+               $"SELECT * FROM Account WHERE Name='{textBox1.Text}' AND Passwort='{textBox2.Text}'");
+
+            if (hasUser != null)
+                foreach (DataRow Row in hasUser.Rows)
+                {
+                    string AccountID = Convert.ToString(Row["Acc_ID"]);
+                    label3.Text = AccountID;
+                }
+
+            else MessageBox.Show("Your password is wrong.");
+        }
+    }
+}
